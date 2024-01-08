9765fc0db38e9334e3fbf3412655551cf115ae76
fl0wl0w <fl0wl0w@cracked-ice.com>
2024-01-08 00:04:52 -0600

    RXCMD and RXEXE

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ This also adds 4 passthrough connection paths so that the ATTiny output buffers 
 
 # Commands
 ### Header
-* 4: Command
 * 4: Index
-### 0x0 Setup
+* 4: Command
+### 0x0 NOP
+### 0x1 Setup
 * 4: Index Assignment
 * 1: PC5 -> PB7 passthrough
 * 1: PC2 -> PC4 passthrough
@@ -34,40 +35,40 @@ This also adds 4 passthrough connection paths so that the ATTiny output buffers 
 * 2: TCB1 Input Port (PORTA, PORTB, PORTC, RESERVED)
 * 8: TCB1 PinMask
 * 8: CRC
-### 0x1 GPIO In Stream
+### 0x2 GPIO In Stream
 * 8: INA
 * 8: INB
 * 8: INC
 * 8: CRC
-### 0x1 GPIO Out
+### 0x2 GPIO Out
 * 8: OUTA
 * 8: OUTB
 * 8: OUTC
 * 8: CRC
-### 0x2 Analog In Stream
+### 0x3 Analog In Stream
 * 8: Byte Length
 * [12 + Log2(decimation+1)]: Analog Value x
 * 8: CRC
-### 0x3 TCA0 PulseWidth Out 0 <br/> 0x4 TCA0 PulseWidth Out 1 <br/> 0x5 TCA0 PulseWidth Out 2 <br/> 0x6 TCA0 PulseWidth In Stream <br/> 0x8 TCB0 PulseWidth In Stream <br/> 0xA TCB1 PulseWidth In Stream
+### 0x4 TCA0 PulseWidth Out 0 <br/> 0x5 TCA0 PulseWidth Out 1 <br/> 0x6 TCA0 PulseWidth Out 2 <br/> 0x7 TCA0 PulseWidth In Stream <br/> 0x9 TCB0 PulseWidth In Stream <br/> 0xB TCB1 PulseWidth In Stream
 * 16: Pulsewidth
 * 8: CRC
-### 0x6 TCA0 Frequency Out <br/> 0x8 TCB0 Frequency In Stream <br/> 0xA TCB1 Frequency In Stream
+### 0x7 TCA0 Frequency Out <br/> 0x9 TCB0 Frequency In Stream <br/> 0xB TCB1 Frequency In Stream
 * 16: Frequency
 * 8: CRC
-### 0x6 TCA0 SENT In Stream (1 value) <br/> 0x8 TCB0 SENT In Stream (1 value) <br/> 0xA TCB1 SENT In Stream (1 value)
+### 0x7 TCA0 SENT In Stream (1 value) <br/> 0x9 TCB0 SENT In Stream (1 value) <br/> 0xB TCB1 SENT In Stream (1 value)
 * 12: Value
 * 4: Reserved
 * 8: CRC
-### 0x7 TCA0 SENT In Stream (2 values) <br/> 0x9 TCB0 SENT In Stream (2 values) <br/> 0xB TCB1 SENT In Stream (2 values)
+### 0x8 TCA0 SENT In Stream (2 values) <br/> 0xA TCB0 SENT In Stream (2 values) <br/> 0xC TCB1 SENT In Stream (2 values)
 * 12: Value1
 * 12: Value2
 * 8: CRC
-### 0xC TCB0 Pulsewidth and Frequency In Stream <br/> 0xD TCB0 Pulsewidth and Frequency In Stream
+### 0xD TCB0 Pulsewidth and Frequency In Stream <br/> 0xE TCB0 Pulsewidth and Frequency In Stream
 * 16: Pulsewidth
 * 16: Frequency
 * 8: CRC
-### 0x8 TCB0 Pulsewidth and Frequency OUT <br/> 0xA TCB1 Pulsewidth and Frequency OUT
+### 0x9 TCB0 Pulsewidth and Frequency OUT <br/> 0xB TCB1 Pulsewidth and Frequency OUT
 * 8: Pulsewidth
 * 8: Frequency
 * 8: CRC
-
+### 0xF NOP
--- expander.s
+++ expander.s
@@ -1,34 +1,230 @@
 #include <avr/io.h>
  
-#define rxstate r2
+#define rxstate r25
+#define rxcmd0 r2
+#define rxcmd1 r3
+#define rxcmd2 r4
+#define rxcmd3 r5
+#define rxcmd4 r6
+#define index r8
 
+.data
+rxbuf: .byte 80
+
+.text
 ; USART0_RCX
 .global __vector_17
 __vector_17:
-SBRC rxstate, 1
-RJMP RX23
-SBRS rxstate, 0
-JMP RX0
-JMP RX1
-RX23:
-SBRS rxstate, 0
-JMP RX2
-JMP RX3
+    ; switch rxstate
+    MOV r16, rxstate
+    LSL r16
+    LDI ZL, lo8(RXStateSwitch)
+    LDI ZH, hi8(RXStateSwitch)
+    ADD ZL, r16
+    ADC ZH, r0
+    ; store char in r16 before switch
+    LDS r16, USART0_RXDATAL
+    IJMP
 
-RX0:
-RETI
-RX1:
-RETI
-RX2:
-RETI
-RX3:
-RETI
+RXStateSwitch:
+    .int RXCMD
+    .int RXFWD3
+    .int RXSTORECMD2
+    .int RXSTORECMD1
+    .int RXFWD4
+    .int RXSTORECMD3
+    .int RXSTORECMD2
+    .int RXSTORECMD1
+    .int RXFWD5
+    .int RXSTORECMD4
+    .int RXSTORECMD3
+    .int RXSTORECMD2
+    .int RXSTORECMD1
+    .int RXEXE
+    .int RXSTORECMD3
+    .int RXSTORECMD2
+    .int RXSTORECMD1
+
+RXSTORECMD1:
+    ; store rxcmd1
+    MOV rxcmd1, r16
+    DEC rxstate
+    RETI
+
+RXSTORECMD2:
+    ; store rxcmd2
+    MOV rxcmd2, r16
+    DEC rxstate
+    RETI
+
+RXSTORECMD3:
+    ; store rxcmd3
+    MOV rxcmd3, r16
+    DEC rxstate
+    RETI
+
+RXSTORECMD4:
+    ; store rxcmd4
+    MOV rxcmd4, r16
+    DEC rxstate
+    RETI
+
+RXFWD3:
+    ;reset state to 0
+    LDI rxstate, 0
+    ; TODO Forward
+    RETI
+
+RXFWD4:
+    ;reset state to 0
+    LDI rxstate, 0
+    ; TODO Forward
+    RETI
+
+RXFWD5:
+    ;reset state to 0
+    LDI rxstate, 0
+    ; TODO Forward
+    RETI
+
+RXEXE:
+    ; store rxcmd4
+    MOV rxcmd4, r16
+    ;reset state to 0
+    LDI rxstate, 0
+    ; read rxcmd0 into r16
+    MOV r16, rxcmd0
+    ;switch execute
+    ANDI r16, 0b00001111
+    LSL r16
+    LDI ZL, lo8(RXEXESwitch)
+    LDI ZH, hi8(RXEXESwitch)
+    ADD ZL, r16
+    ADC ZH, r0
+    IJMP
+
+RXEXESwitch:
+    .int RXNOP
+    .int RXNOP
+    .int RXEXEGPIO
+    .int RXNOP
+    .int RXEXETCA0PW0
+    .int RXEXETCA0PW1
+    .int RXEXETCA0PW2
+    .int RXEXETCA0Frequency
+    .int RXNOP
+    .int RXEXETCB0PWFrequency
+    .int RXNOP
+    .int RXEXETCB1PWFrequency
+    .int RXNOP
+    .int RXNOP
+    .int RXNOP
+    .int RXNOP
+
+RXEXEGPIO:
+    ; TODO
+    RETI
+
+RXEXETCA0PW0:
+    ; TODO
+    RETI
+
+RXEXETCA0PW1:
+    ; TODO
+    RETI
+
+RXEXETCA0PW2:
+    ; TODO
+    RETI
+
+RXEXETCA0Frequency:
+    ; TODO
+    RETI
+
+RXEXETCB0PWFrequency:
+    ; TODO
+    RETI
+
+RXEXETCB1PWFrequency:
+    ; TODO
+    RETI
+
+RXCMD:
+    ; store rxcmd0
+    MOV rxcmd0, r16
+    ;switch command
+    ANDI r16, 0b00001111
+    LSL r16
+    LDI ZL, lo8(RXCMDSwitch)
+    LDI ZH, hi8(RXCMDSwitch)
+    ADD ZL, r16
+    ADC ZH, r0
+    IJMP ; 21 clocks to here
+
+RXCMDSwitch:
+    .int RXNOP
+    .int RXCMDSetup
+    .int RXCMDCheck4
+    .int RXCMDAnalogFwd
+    .int RXCMDCheck3
+    .int RXCMDCheck3
+    .int RXCMDCheck3
+    .int RXCMDCheck3
+    .int RXCMDFwd4
+    .int RXCMDCheck3
+    .int RXCMDFwd4
+    .int RXCMDCheck3
+    .int RXCMDFwd4
+    .int RXCMDFwd5
+    .int RXCMDFwd5
+    .int RXNOP
+
+RXNOP:
+    RETI
+
+RXCMDSetup:
+    ; TODO
+    RETI
+
+RXCMDAnalogFwd:
+    ; TODO
+    RETI
+
+RXCMDCheck3:
+    MOV r16, rxcmd0
+    SWAP r16
+    ANDI r16, 0b00001111
+    CPSE index, r16
+    LDI rxstate, 3
+    LDI rxstate, 15
+    RETI
+
+RXCMDCheck4:
+    MOV r16, rxcmd0
+    SWAP r16
+    ANDI r16, 0b00001111
+    CPSE index, r16
+    LDI rxstate, 7
+    LDI rxstate, 16
+    RETI
+
+RXCMDFwd4:
+    LDI rxstate, 7
+    RETI
+
+RXCMDFwd5:
+    LDI rxstate, 12
+    RETI
+
 
 .global main
 main:
 	ldi r16,0b00100000
 	out VPORTB_DIR,r16
 	out VPORTB_OUT,r16
+    LDI r16, 0
+    MOV r0, r16
+    MOV rxstate, r0
 	rjmp main
 .end
 
